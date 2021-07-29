b72f04e53c2fff3ef19086881b7a47b89b0c47e4
Kayce Williams <kalewill88@gmail.com>
2021-07-29 17:06:09 -0400

    Add Login.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react"
 import './App.css';
+import Login from './components/login';
+// import Navbar from './components/navbar';
+// import Searchbar from'./components/searchbar';
+// import Watchlist from'./components/watchlist';
+import Footer from'./components/footer';
 
 function App() {
   return (
     <div className="App">
-     <div></div>
+      <Login />
+      {/* <Navbar /> */}
+      {/* <Searchbar /> */}
+      {/* <Watchlist /> */}
+      <Footer />
     </div>
   );
 }
