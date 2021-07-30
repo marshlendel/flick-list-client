ae37b07d5d38cd49e961a254dd5e7e48ebcc79b8
Kayce Williams <kalewill88@gmail.com>
2021-07-29 21:49:00 -0400

    Styling Update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
+// import FlickLogo from '../assets/FlickLogo.png';
 import {
-  Navbar,
-  NavbarBrand,
-  Button
+    Navbar,
+    NavbarBrand,
+    Button
 } from 'reactstrap';
 
 const WatchListNav = (props) => {
 
- return (
-    <div>
-    <Navbar style = {{padding: '10px', backgroundColor: 'black', height: '50px', textAlign: 'left'}} fixed='top' bg-primary light expand="md">
-        <NavbarBrand href="/"className="nav" style={{fontFamily: 'sans-serif', color: 'white'}}>FlickList</NavbarBrand>
-        <Button outline color="primary" style={{marginLeft: '40%'}}>My Watch List</Button>{' '}
-        <Button outline color="primary" style={{float: 'right'}}>Logout</Button>{' '}
+    return (
+    <div className="nav-container">
+        <Navbar className="navbar" style = {{padding: '30px', backgroundColor: 'black', height: '50px', textAlign: 'left'}} fixed='top' bg-primary light expand="md">
+        <NavbarBrand className="flick"><b>FlickList</b></NavbarBrand>
+        <Button className="button" outline color="primary" style={{marginLeft: '40%'}}><b>My Watch List</b></Button>{' '}
+        <Button className="button" outline color="primary" style={{float: 'right'}}><b>Logout</b></Button>{' '}
     </Navbar>
     </div>
     );
--- src/components/watchlist.js
+++ src/components/watchlist.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {useState} from "react";
 import {
-  Card, CardText, CardBody,
-  CardTitle,
-  CardColumns,
-  Button
+    Card, CardText, CardBody,
+    CardTitle,
+    CardColumns,
+     Button
 } from 'reactstrap';
 
 
@@ -12,44 +12,44 @@ const List = (props) => {
 const [movies, setMovies] = useState([])
 
 let handleFetches = () => {
-  let url = "https://api.themoviedb.org/3/movie/popular?api_key=e6564d42419f5d069c69139088835a5e&language=en-US&page=1"
+    let url = "https://api.themoviedb.org/3/movie/popular?api_key=e6564d42419f5d069c69139088835a5e&language=en-US&page=1"
 
-  fetch(url, {
+    fetch(url, {
     method: "GET",
     headers: new Headers({
         "Content-type": "application/json",
         "Authorization": props.sessionToken
     })
-  })
-  .then((res) => res.json())
-  .then(json => setMovies(json.results))
-  console.log(movies[0].original_title)
-  .catch(err => console.log(err))
+    })
+    .then((res) => res.json())
+    .then(json => setMovies(json.results))
+    console.log(movies[0].original_title)
+    .catch(err => console.log(err))
 }
     
-  return (
+return (
     <div>
-      <Card style = {{color: 'white'}}>
-        <CardBody>
-          <CardTitle tag="h5">Watch List:</CardTitle>
-        </CardBody>
-        <CardColumns>
-        <img width="100%" src={movies[0]} alt="movie1" />
-        </CardColumns>
-        <CardColumns>
-        <img width="100%" src="/assets/318x180.svg" alt="movie2" />
-        </CardColumns>
-        <CardColumns>
-        <img width="100%" src="/assets/318x180.svg" alt="movie3" />
-        </CardColumns>
+        <Card style = {{color: 'white'}}>
+            <CardBody>
+                <CardTitle tag="h5">Watch List:</CardTitle>
+            </CardBody>
+            <CardColumns>
+                <img width="100%" src={movies[0]} alt="movie1" />
+            </CardColumns>
+            <CardColumns>
+                <img width="100%" src="/assets/318x180.svg" alt="movie2" />
+            </CardColumns>
+            <CardColumns>
+                <img width="100%" src="/assets/318x180.svg" alt="movie3" />
+            </CardColumns>
     
-        <CardBody>
-          <CardText>Movies added from out Fetch will be displayed here</CardText>
-          <Button onClick={handleFetches}>Load My List</Button>
-        </CardBody>
-      </Card>
+            <CardBody>
+            <CardText>Movies added from out Fetch will be displayed here</CardText>
+            <Button className="button" onClick={handleFetches}><b>Load My List</b></Button>
+            </CardBody>
+        </Card>
     </div>
-  );
+    );
 };
 
 export default List;
