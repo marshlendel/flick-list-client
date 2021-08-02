0924680fbe9150eb17fe1d2a7c3f950b06c2ddde
Marshall Brown <marshallendel@gmail.com>
2021-08-02 10:31:54 -0400

    update styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import './App.css';
 import WatchListNav from './components/navbar';
-import Login from './components/login';
+// import List from './components/watchlist';
 import Footer from'./components/footer';
 import Search from "./components/searchbar";
 
@@ -23,7 +23,9 @@ function App() {
       <br />
       <br />
       <Search />
+      {/* <List /> */}
       <Footer />
+
     </div>
   );
 }
