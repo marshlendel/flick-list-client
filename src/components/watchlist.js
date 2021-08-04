3b49114dccdb72a2d79e032e5581be8c0c2bfbb8
Anne Porter <a.porter9212@gmail.com>
2021-08-04 09:17:36 -0400

    cleaned up watchlist

--- src/components/watchlist.js
+++ src/components/watchlist.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useState, useEffect} from "react";
 // import {Container, Row, Col} from 'reactstrap';
-import MovieList from './displaymovies';
 import {
     Card,
     CardBody,
@@ -10,11 +9,37 @@ import {
     Row,
     Col
 } from 'reactstrap';
-import EditMovie from './editmovie';
 
 
 const List = (props) => {
 const [movies, setMovies] = useState([])
+
+let UpdateMovie = () => {
+  fetch(`http://localhost:4000/list/update/${props.EditMovie.id}`, {
+  method: 'PUT',
+  body: JSON.stringify({list: {watched: editWatched}}),
+  headers: new Headers({
+      'Content-Type': 'application/json',
+      'Authorization':  `${props.token}`
+  })
+})
+.then ((res)=> {
+  props.fetchMovies();
+})
+}
+
+let deleteMovie = (movie)=> {
+  fetch(`http://localhost:4000/list/delete/${movie.id}`, {
+      method: 'DELETE',
+      headers: new Headers({
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${props.token}`
+      })
+  })
+  .then (() => {
+      props.fetchMovies()
+  })
+}
 
 let fetchMovies = () => {
     fetch('http://localhost:4000/list/', {
@@ -30,6 +55,8 @@ let fetchMovies = () => {
       return <div key = {movies.id}>
         <Row>
           <Col>{ `${movies.title} (${movies.year})` }</Col>
+          <Col><Button color={color} onClick={() => updateMovie(movies)}></Button></Col>
+          <Col><Button color="danger" onClick={()=> deleteMovie(movies)}>Delete</Button></Col>
         </Row>
       </div>
     }))
