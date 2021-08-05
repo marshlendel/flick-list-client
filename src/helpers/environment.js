0f19828e2e4c1f7c589ec8f0c9726cfed6fa46ad
Marshall Brown <marshallendel@gmail.com>
2021-08-05 13:59:20 -0400

    fix URL again

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case "localhost" || "127.0.0.1":
         APIURL = "http://localhost:4000";
         break;
-    case "flick-list-client-mb.herokuapp.com/":
+    case "flick-list-client-mb.herokuapp.com":
         APIURL= "https://flick-list-mb.herokuapp.com/"
         break;
     default:
